da37b27e757122d1a5ba09bd43bd3758f8a8816c
hemanthu2001 <hemanthu14352892@gmail.com>
2025-01-10 12:00:59 +0530

    Added room booking functionality

--- HotelBookingWebsite/models.py
+++ HotelBookingWebsite/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Room(models.Model):
+    room_no = models.IntegerField(unique=True)
+    room_type = models.CharField(max_length=100)
+    room_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Room {self.room_no} - {self.room_type}"
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"Booking for {self.guest_name} ({self.room})"
+    
--- HotelBookingWebsite/views.py
+++ HotelBookingWebsite/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import *
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def book_room(request, room_id):
+    room = Room.objects.get(id=room_id)
+    if request.method == 'POST':
+        check_in = request.POST.get('check_in')
+        check_out = request.POST.get('check_out')
+        total_price = (check_out - check_in).days * room.room_price
+        Booking.objects.create(user=request.user, room=room, check_in=check_in, check_out=check_out, total_price=total_price)
+        return redirect('my_bookings')
+    return render(request, 'book_room.html', {'room': room})
+
